61f6c5264b248111fdcf9bc10502d971f169df36
hecker <118846180+its-hecker@users.noreply.github.com>
2026-06-29 01:36:34 +0500

    Update recovery_twrp.cpp

--- recovery/recovery_twrp.cpp
+++ recovery/recovery_twrp.cpp
@@ -14,127 +14,27 @@
  * limitations under the License.
  */
 
-#include <stdint.h>
-#include <string.h>
-
-#include <string>
-#include <string_view>
-#include <vector>
-
-#include <android-base/endian.h>
-#include <android-base/logging.h>
-#include <android-base/strings.h>
-#include <app_nugget.h>
-#include <misc_writer/misc_writer.h>
-#include <nos/NuggetClient.h>
-#include <nos/debug.h>
-
 #include "partitions.hpp"
-#include "gui/gui.h"
 
 namespace android {
 namespace device {
 namespace google {
 namespace coral {
 
-namespace {
-
-/** Wipe user data from Titan M. */
-bool WipeTitanM() {
-    // Connect to Titan M
-    ::nos::NuggetClient client;
-    client.Open();
-    if (!client.IsOpen()) {
-        LOG(ERROR) << "Failed to connect to Titan M";
-        return false;
-    }
-
-    // Tell it to wipe user data
-    const uint32_t magicValue = htole32(ERASE_CONFIRMATION);
-    std::vector<uint8_t> magic(sizeof(magicValue));
-    memcpy(magic.data(), &magicValue, sizeof(magicValue));
-    const uint32_t status
-            = client.CallApp(APP_ID_NUGGET, NUGGET_PARAM_NUKE_FROM_ORBIT, magic, nullptr);
-    if (status != APP_SUCCESS) {
-        LOG(ERROR) << "Titan M user data wipe failed: " << ::nos::StatusCodeString(status)
-                   << " (" << status << ")";
-        return false;
-    }
-
-    LOG(INFO) << "Titan M wipe successful";
-    return true;
-}
-
-// Wipes the provisioned flag as part of data wipe.
-bool WipeProvisionedFlag() {
-    // Must be consistent with the one in init.hardware.rc (10-byte `theme-dark`).
-    hardware::google::pixel::MiscWriter misc_writer(hardware::google::pixel::MiscWriterActions::kClearDarkThemeFlag);
-    if (!misc_writer.PerformAction()) {
-        LOG(ERROR) << "Failed to clear the dark theme flag";
-        return false;
-    }
-    LOG(INFO) << "Provisioned flag wiped successful";
-    return true;
-}
-
-// Provision Silent OTA(SOTA) flag while reason is "enable-sota"
-//bool ProvisionSilentOtaFlag(const std::string& reason) {
-//    if (android::base::StartsWith(reason, hardware::google::pixel::MiscWriter::kSotaFlag)) {
-//	hardware::google::pixel::MiscWriter misc_writer(hardware::google::pixel::MiscWriterActions::kSetSotaFlag);
-//        if (!misc_writer.PerformAction()) {
-//            LOG(ERROR) << "Failed to set the silent ota flag";
-//            return false;
-//        }
-//        LOG(INFO) << "Silent ota flag set successful";
-//    }
-//    return true;
-//}
-
-}  // namespace
-
-class CoralDevice : public ::BasePartition
-{
+class CoralDevice : public ::BasePartition {
   public:
     CoralDevice() {}
 
-    /** Hook to wipe user data not stored in /data */
     bool PostWipeEncryption() override {
-        // Try to do everything but report a failure if anything wasn't successful
-        bool totalSuccess = false;
-
-        gui_print("Wiping Titan M...\n");
-
-        uint32_t retries = 5;
-        while (retries--) {
-            if (WipeTitanM()) {
-                totalSuccess = true;
-                break;
-            }
-        }
-
-        if (!WipeProvisionedFlag()) {
-            totalSuccess = false;
-        }
-
-        // Extendable to wipe other components
-
-        // Additional behavior along with wiping data
-        //auto reason = GetReason();
-        //CHECK(reason.has_value());
-        //if (!ProvisionSilentOtaFlag(reason.value())) {
-        //    totalSuccess = false;
-        //}
-
-        return totalSuccess;
+        return true;
     }
 };
 
-}  // namespace pixel
+}  // namespace coral
 }  // namespace google
-}  // namespace hardware
+}  // namespace device
 }  // namespace android
 
-BasePartition *make_partition()
-{
-	    return new ::android::device::google::coral::CoralDevice();
+BasePartition *make_partition() {
+    return new ::android::device::google::coral::CoralDevice();
 }
