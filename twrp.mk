4bd831a1f4439fd0e6451bc3f5b18ec0529a1e07
hecker <118846180+its-hecker@users.noreply.github.com>
2026-06-29 04:57:47 +0500

    Update twrp.mk

--- twrp.mk
+++ twrp.mk
@@ -51,8 +51,3 @@ TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_RESETPROP := true
 TW_USE_FSCRYPT_POLICY := 1
 TW_LOAD_VENDOR_MODULES := "ftm5.ko"
-
-# Force prepdecrypt to set security patches regardless of boot mode
-# Prevents fastboot boot detection from disabling SETPATCH on recovery-as-boot devices
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system.prop:$(TARGET_COPY_OUT_RECOVERY)/root/prop.default
